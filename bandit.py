ab271a7fffd21b455da3ec830c0d67b976bb9e72
Mario Geiger <geiger.mario@gmail.com>
2020-09-22 16:55:03 +0200

    first commit

---
+++ bandit.py
@@ -0,0 +1,89 @@
+# pylint: disable=no-member, invalid-name, not-callable, missing-docstring
+import itertools
+import torch
+
+
+def inv_action(s):
+    return s.replace('A', 'b').replace('B', 'A').replace('b', 'B')
+
+
+def inv_reward(s):
+    return s.replace('1', 'o').replace('0', '1').replace('o', '0')
+
+
+def str_prod(*iterators, n=1):
+    return tuple([''.join(x) for x in itertools.product(*iterators, repeat=n)])
+
+
+def init(n_arms, mem):
+    arms = tuple('ABCDEFGH'[:n_arms])
+    actions = str_prod(arms, '01')
+    states = str_prod('+-', str_prod('01', n=mem))
+    rewards = torch.tensor([1.0 if s[0] == '+' else -1.0 for s in states])
+    return states, actions, arms, rewards
+
+
+def master_matrix(states, actions, prob):
+    """
+    compute the matrix M_{ss,s,a} = P[ss | s, a]
+    """
+    mm = torch.tensor([
+        [
+            [
+                prob(ss, s, a)
+                for a in actions
+            ]
+            for s in states
+        ]
+        for ss in states
+    ], dtype=torch.float64)
+    return mm
+
+
+def transfer_matrix(pi, mm):
+    return torch.einsum('ja,ija->ij', pi, mm)
+
+
+def steadystate(m, eps=1e-6):
+    p = m.new_ones(len(m)) / len(m)
+
+    for _ in itertools.count():
+        m = m @ m
+
+        for _ in range(2):
+            p_ = m @ p
+
+            d = (p_ - p).abs().max().item()
+            p = p_
+
+            if d < eps:
+                break
+        if d < eps:
+            break
+
+    return p
+
+
+def uniform_grid(n):
+    a = 1 / (2 + 2 * n)
+    f = torch.linspace(a, 1.0 - a, n)
+    fs = torch.stack(torch.meshgrid(f, f), dim=-1).reshape(-1, 2)
+    return fs
+
+
+def avg_gain(rewards, mms, eps, pi):
+    g = 0
+    for mm in mms:
+        m = transfer_matrix(pi, mm)
+        p = steadystate(m, eps)
+        g += torch.dot(rewards, p)
+    g = g / len(mms)
+    return g
+
+
+def grad_fn(rewards, mms, eps, w):
+    w = w.clone()
+    w.requires_grad_()
+    g = avg_gain(rewards, mms, eps, w.softmax(1))
+    g.backward()
+    return w.grad.clone()
---
+++ main.py
@@ -0,0 +1,107 @@
+# pylint: disable=no-member, invalid-name, not-callable, missing-docstring, line-too-long
+import argparse
+import os
+import subprocess
+from functools import partial
+
+import torch
+
+from bandit import grad_fn, init, master_matrix
+from gradientflow import gradientflow_ode
+
+
+def prob(f, arms, ss, s, a):
+    # s = +1101
+    # a = A0
+    #ss = -1010
+    if ss[1:] == s[2:] + a[1]:
+        fa = f[arms.index(a[0])]
+        return fa if ss[0] == '+' else 1 - fa
+    return 0
+
+
+def execute(args):
+    torch.set_default_dtype(torch.float64)
+    torch.manual_seed(args.seed)
+
+    states, actions, arms, rewards = init(n_arms=args.arms, mem=args.memory)
+    rewards = rewards.to(device=args.device)
+
+    fs = torch.tensor([
+        [0.5, 0.5 + args.gamma],
+        [0.5, 0.5 - args.gamma],
+        [0.5 + args.gamma, 0.5],
+        [0.5 - args.gamma, 0.5],
+    ])
+
+    mms = [master_matrix(states, actions, partial(prob, f, arms)).to(device=args.device) for f in fs]
+
+    w = torch.randn(len(states), len(actions)).mul(1).to(device=args.device)
+    dynamics = []
+
+    for state, internals in gradientflow_ode(w, partial(grad_fn, rewards, mms, args.eps), max_dgrad=args.max_dgrad):
+
+        state['ngrad'] = internals['gradient'].norm().item()
+        dynamics.append(state)
+
+        if state['step'] == args.step_stop:
+            return {
+                'args': args,
+                'dynamics': dynamics,
+                'pi': internals['variables'].softmax(1),
+            }
+
+
+def main():
+    git = {
+        'log': subprocess.getoutput('git log --format="%H" -n 1 -z'),
+        'status': subprocess.getoutput('git status -z'),
+    }
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--device", type=str, default='cpu')
+
+    parser.add_argument("--seed", type=int, default=0)
+    parser.add_argument("--memory", type=int, required=True)
+    parser.add_argument("--arms", type=int, required=True)
+    parser.add_argument("--gamma", type=float, default=0.1)
+
+    parser.add_argument("--max_dgrad", type=float, default=1e-4)
+    parser.add_argument("--eps", type=float, default=1e-8)
+    parser.add_argument("--step_stop", type=int, default=1000)
+
+    parser.add_argument("--pickle", type=str, required=True)
+    args = parser.parse_args()
+
+    if args.pte is None:
+        args.pte = args.ptr
+
+    if args.chunk is None:
+        args.chunk = max(args.ptr, args.pte, args.ptk)
+
+    if args.max_wall_kernel is None:
+        args.max_wall_kernel = args.max_wall
+
+    if args.tau_over_h_kernel is None:
+        args.tau_over_h_kernel = args.tau_over_h
+
+    if args.seed_init == -1:
+        args.seed_init = args.seed_trainset
+
+    torch.save(args, args.pickle)
+    saved = False
+    try:
+        for res in execute(args):
+            res['git'] = git
+            with open(args.pickle, 'wb') as f:
+                torch.save(args, f)
+                torch.save(res, f)
+                saved = True
+    except:
+        if not saved:
+            os.remove(args.pickle)
+        raise
+
+
+if __name__ == "__main__":
+    main()
