ce8ced24e683897d509b8bc67f0a0d121c06a6d1
Mario Geiger <geiger.mario@gmail.com>
2020-09-28 14:43:50 +0200

    bootstrap_ram

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 # pylint: disable=no-member, invalid-name, not-callable, missing-docstring, line-too-long
 import argparse
-import math
+# import math
 import os
 import subprocess
 from functools import partial
@@ -12,29 +12,12 @@ from bandit import grad_fn, init, master_matrix
 from gradientflow import gradientflow_ode
 
 
-def execute(args):
+def optimize(args, w, mms, rewards, stop_steps, iterator):
     wall = perf_counter()
-    torch.set_default_dtype(torch.float64)
-    torch.manual_seed(args.seed)
-
-    states, actions, arms, rewards, prob = init(n_arms=args.arms, mem=args.memory, mem_type=args.memory_type)
-    rewards = rewards.to(device=args.device)
-
-    assert args.arms == 2
-    fs = torch.tensor([
-        [0.5, 0.5 + args.gamma],
-        [0.5, 0.5 - args.gamma],
-        [0.5 + args.gamma, 0.5],
-        [0.5 - args.gamma, 0.5],
-    ])
-
-    mms = [master_matrix(states, actions, partial(prob, f)).to(device=args.device) for f in fs]
-
-    w = torch.randn(len(states), len(actions), device=args.device).mul(args.std0)
-    dynamics = []
-
     wall_print = perf_counter()
     wall_save = perf_counter()
+
+    dynamics = []
 
     for state, internals in gradientflow_ode(w, partial(grad_fn, rewards, mms, args.reset, args.eps), max_dgrad=args.max_dgrad):
 
@@ -54,22 +37,86 @@ def execute(args):
             wall_save = perf_counter()
             save = True
 
-        if state['step'] == args.step_stop or state['ngrad'] <= args.ngrad_stop:
+        if state['step'] == stop_steps:
             save = True
             stop = True
 
+        r = {
+            'dynamics': dynamics,
+            'weights': internals['variables'],
+            'pi': internals['variables'].softmax(1),
+        }
+
         if save:
+            if iterator:
+                yield r
+
+        if stop:
+            if not iterator:
+                return r
+            else:
+                break
+
+
+def execute(args):
+    torch.set_default_dtype(torch.float64)
+    torch.manual_seed(args.seed)
+
+    states, actions, arms, rewards, prob = init(n_arms=args.arms, mem=args.memory, mem_type=args.memory_type)
+    rewards = rewards.to(device=args.device)
+
+    assert args.arms == 2
+    fs = torch.tensor([
+        [0.5, 0.5 + args.gamma],
+        [0.5, 0.5 - args.gamma],
+        [0.5 + args.gamma, 0.5],
+        [0.5 - args.gamma, 0.5],
+    ])
+
+    mms = [master_matrix(states, actions, partial(prob, f)).to(device=args.device) for f in fs]
+
+    w = torch.randn(len(states), len(actions), device=args.device).mul(args.std0)
+
+    rs = [optimize(args, w, mms, rewards, args.trials_steps, iterator=False) for _ in range(args.trials)]
+    r = max(rs, lambda x: x['dynamics'][-1]['gain'])
+
+    for r in optimize(args, r['weights'], mms, rewards, args.stop_steps, iterator=True):
+        yield {
+            'args': args,
+            'states': states,
+            'actions': actions,
+            'arms': arms,
+            'trials': rs,
+            'main': r,
+            'ram': None,
+        }
+
+    if args.bootstrap_ram == 1:
+        if args.memory_type == 'shift':
+            states2, actions2, arms2, rewards2, prob2 = init(n_arms=args.arms, mem=args.memory, mem_type='ram')
+            assert states2 == states
+            assert arms2 == arms
+
+            mms2 = [master_matrix(states2, actions2, partial(prob2, f)).to(device=args.device) for f in fs]
+            rewards2 = rewards2.to(device=args.device)
+
+        w = r['weights']
+        w2 = torch.ones(len(states2), len(actions2)).mul(-100)
+        for s, line in zip(states, w):
+            for a, x in zip(actions, line):
+                a = a[0] + s[2:] + a[1]
+                w2[s, actions2.index(a)] = x
+
+        for r2 in optimize(args, r['weights'], mms2, rewards2, args.stop_steps, iterator=True):
             yield {
                 'args': args,
                 'states': states,
                 'actions': actions,
                 'arms': arms,
-                'dynamics': dynamics,
-                'pi': internals['variables'].softmax(1),
+                'trials': rs,
+                'main': r,
+                'ram': r2,
             }
-
-        if stop:
-            return
 
 
 def main():
@@ -82,6 +129,7 @@ def main():
     parser.add_argument("--device", type=str, default='cpu')
 
     parser.add_argument("--memory_type", type=str, required=True)
+    parser.add_argument("--bootstrap_ram", type=int, default=0)
     parser.add_argument("--memory", type=int, required=True)
     parser.add_argument("--arms", type=int, default=2)
     parser.add_argument("--gamma", type=float, default=0.1)
@@ -89,12 +137,16 @@ def main():
 
     parser.add_argument("--seed", type=int, default=0)
 
+    parser.add_argument("--bootstrap", type=str, default='none')
     parser.add_argument("--max_dgrad", type=float, default=1e-4)
     parser.add_argument("--eps", type=float, default=1e-8)
     parser.add_argument("--std0", type=float, default=1)
 
-    parser.add_argument("--step_stop", type=int, default=math.inf)
-    parser.add_argument("--ngrad_stop", type=float, default=0.0)
+    parser.add_argument("--trials", type=int, default=1)
+    parser.add_argument("--trials_steps", type=int, default=0)
+
+    parser.add_argument("--stop_steps", type=int, default=1000)
+    # parser.add_argument("--stop_ngrad", type=float, default=0.0)
 
     parser.add_argument("--pickle", type=str, required=True)
     args = parser.parse_args()
