21708344ae894a716406ce11dd74d514295cd77b
Mario Geiger <geiger.mario@gmail.com>
2021-05-25 17:20:45 +0200

    prepare for release

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # bandit
+
+## requirements
+
+- [`pytorch=1.8.1`](https://github.com/pytorch/pytorch/releases/tag/v1.8.1)
+- [`gradientflow=0.0.1`](https://github.com/mariogeiger/gradientflow/releases/tag/0.0.1)
--- main.py
+++ main.py
@@ -1,16 +1,122 @@
-# pylint: disable=no-member, invalid-name, not-callable, missing-docstring, line-too-long
 import argparse
-# import math
+import collections
+import itertools
 import os
+import pickle
 import subprocess
 from functools import partial
 from time import perf_counter
-import pickle
 
 import torch
-
-from bandit import grad_fn, init, master_matrix, steadystate, transfer_matrix
 from gradientflow import flow
+
+
+def str_prod(*iterators, n=1):
+    return tuple([''.join(x) for x in itertools.product(*iterators, repeat=n)])
+
+
+def init(n_arms, mem, mem_type):
+    arms = tuple('ABCDEFGH'[:n_arms])
+
+    n_init_states = 0
+
+    if mem_type == 'ram':
+        mem_states = [f"{i:02d}" for i in range(mem)]
+        actions = str_prod(mem_states, arms)
+        states = str_prod(mem_states, ['  ']) + str_prod(mem_states, arms, '+-')
+        n_init_states = len(mem_states)
+        rewards = torch.tensor([{'+': +1.0, '-': -1.0, ' ': 0.0}[s[-1]] for s in states])
+        def prob(f, ss, _s, a):
+            # s = 44A+
+            # a = 32B
+            #ss = 32B-
+            if ss[:-1] == a:
+                fa = f[arms.index(a[-1])]
+                return fa if ss[-1] == '+' else 1 - fa
+            return 0
+
+    elif mem_type == 'actions':
+        actions = arms
+        states = []
+        for n in range(mem + 1):
+            states += str_prod([" " * (mem - n)], str_prod(arms, n=n), [" " * (mem - n)], str_prod("+-", n=n))
+        n_init_states = 1
+        rewards = torch.tensor([{'+': +1.0, '-': -1.0, ' ': 0.0}[s[-1]] for s in states])
+        def prob(f, ss, s, a):
+            # s = AAB++-
+            # a = B
+            #ss = ABB+--
+            if ss[:mem] == s[1:mem] + a and ss[mem:-1] == s[mem + 1:]:
+                fa = f[arms.index(a)]
+                return fa if ss[-1] == '+' else 1 - fa
+            return 0
+
+    if n_init_states == 0:
+        n_init_states = len(states)
+
+    return states, actions, arms, rewards, prob, n_init_states
+
+
+def master_matrix(states, actions, prob):
+    """
+    compute the matrix M_{ss,s,a} = P[ss | s, a]
+    """
+    mm = torch.tensor([
+        [
+            [
+                prob(ss, s, a)
+                for a in actions
+            ]
+            for s in states
+        ]
+        for ss in states
+    ], dtype=torch.float64)
+    return mm
+
+
+def transfer_matrix(pi, mm, reset, p0):
+    r = pi.new_zeros(len(pi), len(pi))
+    r[:len(p0), :] = p0[:, None]
+
+    m = torch.einsum('ja,ija->ij', pi, mm)
+    m = (1 - reset) * m + reset * r
+    return m
+
+
+def steadystate(m, eps=1e-6):
+    for _ in itertools.count():
+        m = m @ m
+
+        if (m - m[:, [0]]).abs().max().item() < eps:
+            return m[:, 0]
+
+        if not torch.isfinite(m).all():
+            return m[:, 0]
+
+
+def avg_gain(rewards, mms, reset, eps, pi, p0):
+    g = 0
+    for mm in mms:
+        m = transfer_matrix(pi, mm, reset, p0)
+        p = steadystate(m, eps)
+        g += torch.dot(rewards, p)
+    g = g / len(mms)
+    return g
+
+
+def grad_fn(rewards, mms, reset, eps, w_pi, w_p0):
+    w_pi = w_pi.clone()
+    w_pi.requires_grad_()
+    pi = w_pi.softmax(1)
+
+    w_p0 = w_p0.clone()
+    w_p0.requires_grad_()
+    p0 = w_p0.softmax(0)
+
+    g = avg_gain(rewards, mms, reset, eps, pi, p0)
+    g.backward()
+
+    return collections.namedtuple("Return", "gain, pi_grad, p0_grad")(g.item(), w_pi.grad.clone(), w_p0.grad.clone())
 
 
 def flow_ode(x, grad_fun, max_dgrad):
@@ -237,18 +343,6 @@ def execute(args):
 
     mms = [master_matrix(states, actions, partial(prob, f)).to(device=args['device']) for f in fs]
 
-    # trials_steps = args['trials_steps']
-    # rs = [last(optimize(args, *w_pi_p0(args, states, actions, n_init_states), mms, rewards, trials_steps, prefix="TRIAL{}/{} ".format(i, args['trials']))) for i in range(args['trials'])]
-
-    # while len(rs) > 1:
-    #     rs = sorted(rs, key=lambda r: r['dynamics'][-1]['gain'])
-    #     print('best gain = {:.3f}'.format(rs[-1]['dynamics'][-1]['gain']))
-    #     rs = rs[len(rs) // 2:]
-    #     if len(rs) == 1:
-    #         break
-    #     trials_steps = round(trials_steps * 1.5)
-    #     rs = [last(optimize(args, r['w_pi'], r['w_p0'], mms, rewards, trials_steps, prefix="TRIAL{}/{} ".format(i, len(rs)))) for i, r in enumerate(rs)]
-
     for r in optimize(args, *w_pi_p0(args, states, actions, n_init_states), mms, rewards):
         yield {
             'args': args,
@@ -288,9 +382,6 @@ def main():
     parser.add_argument("--eps", type=float, default=1e-8)
     parser.add_argument("--eps_init", type=float, default=1e-4)
     parser.add_argument("--std0", type=float, default=1)
-
-    # parser.add_argument("--trials", type=int, default=1)
-    # parser.add_argument("--trials_steps", type=int, default=0)
 
     parser.add_argument("--stop_steps", type=int)
     parser.add_argument("--stop_wall", type=float)
